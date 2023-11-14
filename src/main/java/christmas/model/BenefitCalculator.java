39c82fcf9b5ec5ad42343f1abbd03dbdd0f5af4e
동훈 <donghun@donghun-ui-MacBookPro.local>
2023-11-14 14:06:30 +0900

    feat: 혜택 내역을 계산하는 로직, 출력하는 로직 구현

--- src/main/java/christmas/model/BenefitCalculator.java
+++ src/main/java/christmas/model/BenefitCalculator.java
@@ -1,5 +1,6 @@
 package christmas.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class BenefitCalculator {
@@ -13,6 +14,11 @@ public class BenefitCalculator {
     private static int SPECIAL_SALE_PRICE = 0;
 
     private static String PRESENT = "없음";
+    private static int PRESENT_PRICE = 0;
+
+
+    private static Map<String, Integer> TOTAL_BENEFIT = new HashMap<>();
+    private static int TOTAL_BENEFIT_PRICE = 0;
 
     private static UserOrder userOrder;
 
@@ -20,14 +26,31 @@ public class BenefitCalculator {
         this.userOrder = userOrder;
     }
 
-    public int calculateTotalPrice() {
+    public int getTotalPrice() {
         userOrder.getReservationOrder().forEach((key, value) -> {
             TOTAL_PRICE += Menu.getPrice(key) * value;
         });
         return TOTAL_PRICE;
     }
 
-    public int calculateDdaySalePrice() {
+    public String getPresent() {
+        if (isPresentAvailable()) {
+            PRESENT = "샴페인";
+        }
+        return PRESENT;
+    }
+
+    public Map<String, Integer> calculateTotalBenefit() {
+        TOTAL_BENEFIT.put("크리스마스 디데이 할인: ", calculateDdaySalePrice());
+        TOTAL_BENEFIT.put("평일 할인: ", calculateWeekdaySale());
+        TOTAL_BENEFIT.put("주말 할인: ", calculateWeekendSale());
+        TOTAL_BENEFIT.put("특별 할인: ", calculateSpecialSalePrice());
+        TOTAL_BENEFIT.put("증정 이벤트: ", calculatePresentPrice());
+
+        return TOTAL_BENEFIT;
+    }
+
+    private int calculateDdaySalePrice() {
         int date = userOrder.getReservationDate();
         if (date <= 25) {
             D_DAY_SALE_PRICE += (date - 1) * 100;
@@ -35,27 +58,40 @@ public class BenefitCalculator {
         return D_DAY_SALE_PRICE;
     }
 
-    public int calculateDailySalePrice() {
-        if (isWeekend()) {
-            calculateWeekendSale();
-            return WEEKEND_SALE_PRICE;
-        }
-        calculateWeekdaySale();
-        return WEEKDAY_SALE_PRICE;
-    }
-
-    public int calculateSpecialSalePrice() {
+    private int calculateSpecialSalePrice() {
         if (isSpecialDay()) {
             SPECIAL_SALE_PRICE += 1000;
         }
         return SPECIAL_SALE_PRICE;
     }
 
-    public String calculatePresent() {
-        if (isPresentAvailable()) {
-            PRESENT = "샴페인";
+    private int calculateWeekdaySale() {
+        if (!isWeekend()) {
+            userOrder.getReservationOrder().forEach((key, value) -> {
+                if (Menu.isDessert(key)) {
+                    WEEKDAY_SALE_PRICE += value * 2023;
+                }
+            });
         }
-        return PRESENT;
+        return WEEKDAY_SALE_PRICE;
+    }
+
+    private int calculateWeekendSale() {
+        if (isWeekend()) {
+            userOrder.getReservationOrder().forEach((key, value) -> {
+                if (Menu.isMainDish(key)) {
+                    WEEKEND_SALE_PRICE += value * 2023;
+                }
+            });
+        }
+        return WEEKEND_SALE_PRICE;
+    }
+
+    private int calculatePresentPrice() {
+        if (isPresentAvailable()) {
+            PRESENT_PRICE = 25000;
+        }
+        return PRESENT_PRICE;
     }
 
     private boolean isSpecialDay() {
@@ -63,25 +99,9 @@ public class BenefitCalculator {
         return date % 7 == 3 || date == 25;
     }
 
-    private void calculateWeekdaySale() {
-        userOrder.getReservationOrder().forEach((key, value) -> {
-            if (Menu.isDessert(key)) {
-                WEEKDAY_SALE_PRICE += value * 2023;
-            }
-        });
-    }
-
-    private void calculateWeekendSale() {
-        userOrder.getReservationOrder().forEach((key, value) -> {
-            if (Menu.isMainDish(key)) {
-                WEEKEND_SALE_PRICE += value * 2023;
-            }
-        });
-    }
-
     private boolean isWeekend() {
         int date = userOrder.getReservationDate();
-        return date % 7 == 1 || date % 7 == 2;
+        return date % 7 != 1 && date % 7 != 2;
     }
 
     public boolean isBenefitAvailable() {
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -10,20 +10,33 @@ public class OutputView {
         printTotalPrice(benefitCalculator);
         printPresent(benefitCalculator);
         if (benefitCalculator.isBenefitAvailable()) {
-
+            printBenefitResult(benefitCalculator);
         }
+    }
+
+    private void printBenefitResult(BenefitCalculator benefitCalculator) {
+        System.out.println();
+        System.out.println("<혜택 내역>");
+        if (benefitCalculator.isBenefitAvailable()) {
+            benefitCalculator.calculateTotalBenefit().forEach((key, value) -> {
+                if (value != 0) {
+                    System.out.println(key + "-" + value + "원");
+                }
+            });
+        }
+
     }
 
     private void printPresent(BenefitCalculator benefitCalculator) {
         System.out.println();
         System.out.println("<증정 메뉴>");
-        System.out.println(benefitCalculator.calculatePresent());
+        System.out.println(benefitCalculator.getPresent() + " 1개");
     }
 
     private void printTotalPrice(BenefitCalculator benefitCalculator) {
         System.out.println();
         System.out.println("<할인 전 총주문 금액>");
-        int totalPrice = benefitCalculator.calculateTotalPrice();
+        int totalPrice = benefitCalculator.getTotalPrice();
         //쉼표 구분해서 출력 구현 필요
         System.out.println(totalPrice + "원");
     }
