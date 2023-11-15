fb0eb5679e457af256c7bbb7038c38cbc2f09086
동훈 <donghun@donghun-ui-MacBookPro.local>
2023-11-15 15:17:34 +0900

    test: 출력되는 금액을 포매팅하는 메서드 테스트 작성

---
+++ src/test/java/christmas/view/InputViewTest.java
@@ -0,0 +1,16 @@
+package christmas.view;
+
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+
+    InputView inputView = new InputView();
+
+    @Test
+    void readDate() {
+    }
+
+    @Test
+    void readOrder() {
+    }
+}
